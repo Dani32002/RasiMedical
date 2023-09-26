6deebb7a89eb89a703964ef3ee8b45376cac454f
carvafel <m.carvajalp@uniandes.edu.co>
2023-09-26 09:00:39 -0500

    Cambiesitos2

--- rasiMedical/inventario/urls.py
+++ rasiMedical/inventario/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dispositivos_view, name='dispositivos_view'), # type: ignore
-    path('<int:id>', views.dispositivo_view, name = 'elemento_view') # type: ignore
+    path('<int:id>', views.dispositivo_view, name = 'elemento_view'), # type: ignore
+    path('', views.medicamentos_view, name='medicamentos_view'), # type: ignore
+    path('<int:id>', views.medicamento_view, name = 'medicamento_view') # type: ignore
 ]
