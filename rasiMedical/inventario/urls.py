5f9f4af72dd51be3534b220cdde2af55dbee206c
carvafel <m.carvajalp@uniandes.edu.co>
2023-09-26 11:23:04 -0500

    CRUD Medicamentos

--- rasiMedical/inventario/urls.py
+++ rasiMedical/inventario/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dispositivos_view, name='dispositivos_view'), # type: ignore
-    path('<int:id>', views.dispositivo_view, name = 'elemento_view'), # type: ignore
-    path('', views.medicamentos_view, name='medicamentos_view'), # type: ignore
-    path('<int:id>', views.medicamento_view, name = 'medicamento_view') # type: ignore
+    path('dispositivos/', views.dispositivos_view, name='dispositivos_view'), # type: ignore
+    path('dispositivos/<int:id>', views.dispositivo_view, name = 'elemento_view'), # type: ignore
+    path('medicamentos/', views.medicamentos_view, name='medicamentos_view'), # type: ignore
+    path('medicamentos/<int:id>', views.medicamento_view, name = 'medicamento_view') # type: ignore
 ]
--- rasiMedical/inventario/views.py
+++ rasiMedical/inventario/views.py
@@ -24,11 +24,25 @@ def medicamentos_view(request):
         elementos_dto = el.get_medicamentos()
         elementos = serializers.serialize('json', elementos_dto)    
         return HttpResponse(elementos, 'application/json')
-
-@csrf_exempt
-def medicamento_view(request, pk):
-    if request.method == 'GET':
-        elemento_dto = el.get_medicamento(pk)
+    
+    elif request.method == 'POST':
+        elemento_dto = el.create_medicamento(json.loads(request.body))
         elemento = serializers.serialize('json', [elemento_dto,])
         return HttpResponse(elemento, 'application/json')
+
+@csrf_exempt
+def medicamento_view(request, id):
+    if request.method == 'GET':
+        elemento_dto = el.get_medicamento(id)
+        elemento = serializers.serialize('json', [elemento_dto,])
+        return HttpResponse(elemento, 'application/json')
+    
+    elif request.method == 'PUT':
+        elemento_dto = el.update_medicamento(id, json.loads(request.body))
+        elemento = serializers.serialize('json', [elemento_dto,])
+        return HttpResponse(elemento, 'application/json')
+    
+    elif request.method == 'DELETE':
+        el.delete_medicamento(id)
+        return HttpResponse("Borrado exitoso con id: " + str(id))
     
