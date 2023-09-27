e9467397762081208f4c1547e0d650d4e6cfa44d
Luisfetoga2 <l.torresg@uniandes.edu.co>
2023-09-27 00:33:36 -0500

    CRUD Insumos

--- rasiMedical/inventario/urls.py
+++ rasiMedical/inventario/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('dispositivos/', views.dispositivos_view, name='dispositivos_view'), # type: ignore
     path('dispositivos/<int:id>', views.dispositivo_view, name = 'elemento_view'), # type: ignore
     path('medicamentos/', views.medicamentos_view, name='medicamentos_view'), # type: ignore
-    path('medicamentos/<int:id>', views.medicamento_view, name = 'medicamento_view') # type: ignore
+    path('medicamentos/<int:id>', views.medicamento_view, name = 'medicamento_view'), # type: ignore
+    path('insumos/', views.insumos_view, name='insumos_view'), # type: ignore
+    path('insumos/<int:id>', views.insumo_view, name = 'insumo_view') # type: ignore
 ]
