e1c409517245302347018d4d94a2bbf8919cfad2
Daniel Escalante <d.escalante@uniandes.edu.co>
2023-12-05 09:05:01 -0500

    Autorizacion

--- rasiMedical/rasiMedical/views.py
+++ rasiMedical/rasiMedical/views.py
@@ -9,97 +9,112 @@ from usuario.logic import usuario_logic
 from usuario import views as uv
 from inventario import views
 from django.contrib.auth.decorators import login_required
-from rasiMedical.auth0backend import getToken
+from rasiMedical.auth0backend import getRole, getToken
 
 def home(request):
     return render(request, 'home.html')
 
 @login_required
 def asignar(request):
-    return render(request, 'asignar.html')
+    role = getRole(request)
+    if role == "Administrativo":
+        return render(request, 'asignar.html')
+    return HttpResponse("No Autorizado")
 
 @csrf_exempt
 @login_required
 def mostrarTipo(request):
-    rMeds = requests.get("http://10.128.0.6:8080/usuario/medico/", headers={"Accept":"application/json"})
-    medicos = rMeds.json()
-    if request.method == 'POST':
-        if request.POST["elemento"] == "Dispositivo":
-            r = requests.get("http://10.182.0.6:8080/inventario/dispositivo/", headers={"Accept":"application/json"})
-            disps = r.json()
-            print(disps)
-            template = loader.get_template('mostrarDispositivos.html')
-            context = {
-                "dispositivos": disps,
-                "medicos": medicos
-            }
-            return HttpResponse(template.render(context, request))
-        elif request.POST["elemento"] == "Insumo":
-            r = requests.get("http://10.182.0.6:8080/inventario/insumo/", headers={"Accept":"application/json"})
-            insus = r.json()
-            template = loader.get_template('mostrarInsumos.html')
-            context = {
-                "insumos": insus,
-                "medicos": medicos
-            }
-            return HttpResponse(template.render(context, request))
-        else:
-            r = requests.get("http://10.182.0.6:8080/inventario/medicamento/", headers={"Accept":"application/json"})
-            meds = r.json()
-            template = loader.get_template('mostrarMedicamentos.html')
-            context = {
-                "medicamentos": meds,
-                "medicos": medicos
-            }
-            return HttpResponse(template.render(context, request))
+    role = getRole(request)
+    if role == "Administrativo":
+        rMeds = requests.get("http://10.128.0.6:8080/usuario/medico/", headers={"Accept":"application/json"})
+        medicos = rMeds.json()
+        if request.method == 'POST':
+            if request.POST["elemento"] == "Dispositivo":
+                r = requests.get("http://10.182.0.6:8080/inventario/dispositivo/", headers={"Accept":"application/json"})
+                disps = r.json()
+                print(disps)
+                template = loader.get_template('mostrarDispositivos.html')
+                context = {
+                    "dispositivos": disps,
+                    "medicos": medicos
+                }
+                return HttpResponse(template.render(context, request))
+            elif request.POST["elemento"] == "Insumo":
+                r = requests.get("http://10.182.0.6:8080/inventario/insumo/", headers={"Accept":"application/json"})
+                insus = r.json()
+                template = loader.get_template('mostrarInsumos.html')
+                context = {
+                    "insumos": insus,
+                    "medicos": medicos
+                }
+                return HttpResponse(template.render(context, request))
+            else:
+                r = requests.get("http://10.182.0.6:8080/inventario/medicamento/", headers={"Accept":"application/json"})
+                meds = r.json()
+                template = loader.get_template('mostrarMedicamentos.html')
+                context = {
+                    "medicamentos": meds,
+                    "medicos": medicos
+                }
+                return HttpResponse(template.render(context, request))
+    return HttpResponse("No Autorizado")
 
 @csrf_exempt
 @login_required
 def asociarInsumo(request):
-    obj = {
-        "tipo": "Insumo",
-        "fecha": str(date.today()),
-        "activo": True,
-        "elemento": request.POST["insumo"],
-        "medico": request.POST["medico"]
-    }
-    accessToken = getToken(request)
-    print(accessToken)
-    headers = {'authorization': 'Bearer ' + accessToken}
-    requests.post("http://10.182.0.7:8000/asignacion", json = obj, headers=headers)
-    return render(request, 'asignar.html')
+    role = getRole(request)
+    if role == "Administrativo":
+        obj = {
+            "tipo": "Insumo",
+            "fecha": str(date.today()),
+            "activo": True,
+            "elemento": request.POST["insumo"],
+            "medico": request.POST["medico"]
+        }
+        accessToken = getToken(request)
+        print(accessToken)
+        headers = {'authorization': 'Bearer ' + accessToken}
+        requests.post("http://10.182.0.7:8000/asignacion", json = obj, headers=headers)
+        return render(request, 'asignar.html')
+    return HttpResponse("No Autorizado")
 
 @csrf_exempt
 @login_required
 def asociarMedicamento(request):
-    obj = {
-        "tipo": "Medicamento",
-        "fecha": str(date.today()),
-        "activo": True,
-        "elemento": request.POST["medicamento"],
-        "medico": request.POST["medico"]
-    }
-    accessToken = getToken(request)
-    print(accessToken)
-    headers = {'authorization': 'Bearer ' + accessToken}
-    requests.post("http://10.182.0.7:8000/asignacion", json = obj, headers=headers)
-    return render(request, 'asignar.html')
+    role = getRole(request)
+    if role == "Administrativo":
+        obj = {
+            "tipo": "Medicamento",
+            "fecha": str(date.today()),
+            "activo": True,
+            "elemento": request.POST["medicamento"],
+            "medico": request.POST["medico"]
+        }
+        accessToken = getToken(request)
+        print(accessToken)
+        headers = {'authorization': 'Bearer ' + accessToken}
+        requests.post("http://10.182.0.7:8000/asignacion", json = obj, headers=headers)
+        return render(request, 'asignar.html')
+    return HttpResponse("No Autorizado")
 
 @csrf_exempt
 @login_required
 def asociarDispositivo(request):
-    obj = {
-        "tipo": "Dispositivo",
-        "fecha": str(date.today()),
-        "activo": True,
-        "elemento": request.POST["dispositivo"],
-        "medico": request.POST["medico"]
-    }
-    accessToken = getToken(request)
-    print(accessToken)
-    headers = {'authorization': 'Bearer ' + accessToken}
-    requests.post("http://10.182.0.7:8000/asignacion", json = obj, headers=headers)
-    return render(request, 'asignar.html')
+    role = getRole(request)
+    if role == "Administrativo":
+        obj = {
+            "tipo": "Dispositivo",
+            "fecha": str(date.today()),
+            "activo": True,
+            "elemento": request.POST["dispositivo"],
+            "medico": request.POST["medico"]
+        }
+        accessToken = getToken(request)
+        print(accessToken)
+        headers = {'authorization': 'Bearer ' + accessToken}
+        requests.post("http://10.182.0.7:8000/asignacion", json = obj, headers=headers)
+        return render(request, 'asignar.html')
+    return HttpResponse("No Autorizado")
 
 
 @csrf_exempt
